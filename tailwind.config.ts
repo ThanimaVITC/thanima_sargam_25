5cf2a4eb680a461b68cdccf0eb472be3c6ccccb1
ADITHYAN P <mail.adithyanp@gmail.com>
2025-08-23 19:43:35 +0000

    use TAN Kindred as the font for the headings and the counter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
     extend: {
       fontFamily: {
         body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
-        display: ['Anton', 'sans-serif'],
+        headline: ['"TAN-Kindred"', 'sans-serif'],
+        display: ['"TAN-Kindred"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
